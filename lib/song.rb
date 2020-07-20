6f6017b4c850352198f9e49be018d6c5b3ef0e99
An Pham <anpham148@gmail.com>
2020-07-20 10:39:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 class Song
   attr_accessor :artist, :name, :genre
   @@all = []
-  def initialize(name, genre)
+  def initialize(name, genre, artist)
     @name = name
     @genre = genre
+    @artist = artist
     @@all << self
   end
 end
